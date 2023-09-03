0903b63986ea84ff9bcae27b6674f0b7bf7e3a44
EduardoZaRo <irvin.zavala@uabc.edu.mx>
2023-09-02 23:39:05 -0700

    Class 1 Components

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div>
+            Footer goes here
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import "./navbar.css";
+function Navbar() {
+    return (
+        <div className="navbar">
+            Menu goes here
+        </div>
+    );
+}
+
+export default Navbar;
