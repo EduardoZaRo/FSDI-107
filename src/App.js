16212715dc8ddc5bbdb8dbe2378446bec11ac491
EduardoZaRo <irvin.zavala@uabc.edu.mx>
2023-09-13 21:42:39 -0700

    Class 1 and 2 (Intro)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
-import QuantityPicker from './components/quantityPicker';
 import Catalog from './pages/catalog';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
